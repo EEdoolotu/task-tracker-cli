99686d0de19669641ce158d3b402bf70766f47b0
EEdoolotu <ejirogheneedoolotu@gmail.com>
2025-11-26 14:08:53 +0000

    update

--- functions.py
+++ functions.py
@@ -16,11 +16,32 @@ def list_tasks(tasks, args):
         else:
             print(f'{task["id"]}. {pending} {task["title"]}')
 
-def complete_task(args):
-    pass
+def complete_task(tasks, args):
+    task_id = int(args[0])
+    for task in tasks:
+        if task["id"] == task_id:
+            task["completed"] = True
+            print(f'Task {task_id} marked as complete.')
+            return
+        
+    print("Could not find task.")
 
-def delete_task(args):
-    pass
+def delete_task(tasks, args):
+    if not args:
+        print("Please provide a task ID to delete.")
+        return
+
+    try:
+        task_id = int(args[0])
+    except ValueError:
+        print("Please provide a valid number.")
+        return
+    for i, task in enumerate(tasks):
+        if task["id"] == task_id:
+            tasks.pop(i)
+            print(f'Task {task_id} deleted')
+            return
+    print(f'Could not find task with ID {task_id}')
 
 def edit_task(args):
     pass
